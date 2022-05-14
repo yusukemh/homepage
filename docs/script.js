565ad8601adc1b40cc1fb046e2575e7aab82e2c8
yusukemh <yusukemh@hawaii.edu>
2022-05-14 00:01:24 -1000

    some pages added, page transition

--- docs/script.js
+++ docs/script.js
@@ -1,14 +1,16 @@
-$(function () {
-    $("#header").load("header.html");
+// $(function () {
+//     $("#header").load("header.html");
 
-    $("#short_me").load("short_me.html")
-    $("#short_notes").load("short_notes.html")
-    $('#short_projects').load('short_projects.html')
-    $('#short_archive').load('short_archive.html')
-    $('#short_resume').load('short_resume.html')
+//     $('#page_home').load("page_home.html");
 
-    $("#footer").load("footer.html");
-})
+//     // $("#short_me").load("short_me.html")
+//     // $("#short_notes").load("short_notes.html")
+//     // $('#short_projects').load('short_projects.html')
+//     // $('#short_archive').load('short_archive.html')
+//     // $('#short_resume').load('short_resume.html')
+
+//     $("#footer").load("footer.html");
+// })
 
 if (window.MathJax) {
     MathJax.Hub.Queue(
@@ -34,29 +36,40 @@ if (window.MathJax) {
 // });
 
 
-// function switch_content(pushed) {
-//     var home = document.getElementById("home");
-//     var resume = document.getElementById("resume");
-//     var notes = document.getElementById("notes");
-//     var history = document.getElementById("history");
+function switch_content(pushed) {
+    var home = document.getElementById("long_home");
+    var me = document.getElementById("long_me");
+    var notes = document.getElementById("long_notes");
+    var projects = document.getElementById("projects");
+    var archive = document.getElementById("archive");
+    var resume = document.getElementById("resume");
 
-//     contents = [home, resume, notes, history]
 
-//     for (i=0; i<contents.length; i ++) {
-//         contents[i].style.display = 'none';
-//     }
 
-//     switch(pushed) {
-//         case 'home':
-//             home.style.display = "block";
-//             break;
-//         case 'resume':
-//             resume.style.display = 'block';
-//             break;
-//         case 'notes':
-//             notes.style.display = 'block';
-//             break;
-//         case 'history':
-//             history.style.display = 'block';
-//     } 
-// }
+    contents = [home, me, notes, projects, archive, resume]
+
+    for (i = 0; i < contents.length; i++) {
+        contents[i].style.display = 'none';
+    }
+
+    switch (pushed) {
+        case 'home':
+            home.style.display = "block";
+            break;
+        case 'me':
+            me.style.display = "block";
+            break;
+        case 'notes':
+            notes.style.display = "block";
+            break;
+        case 'projects':
+            projects.style.display = "block";
+            break;
+        case 'archive':
+            archive.style.display = "block";
+            break;
+        case 'resume':
+            resume.style.display = 'block';
+            break;
+    }
+}
