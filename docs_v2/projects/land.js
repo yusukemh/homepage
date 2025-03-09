5a69e2bf603fbee9ff499270487b77612b1a331a
yusukemh <yusukemh@hawaii.edu>
2025-03-08 17:23:47 -1000

    major updates in v2

---
+++ docs_v2/projects/land.js
@@ -0,0 +1,23 @@
+var projects = [
+    {
+        "project_name": "Location Agnostic Neural Downscaler",
+        "project_id": "project_land",
+        "summary": "Deep learning model for monthly rainfall interpolation in Hawai`i.",
+        "abstract": "",
+        "img": "",
+    },
+    {
+        "project_name": "Topographical Gaussian Process",
+        "project_id": "project_tgp",
+        "summary": "Gaussian Process with topography-aware unisotropic kernel for rainfall interpolation",
+        "abstract": "",
+        "img": "",
+    },
+    {
+        "project_name": "Diffusion Models for High-resolution Solar Irradiance",
+        "project_id": "project_nimbus_diffusion",
+        "summary": "Diffusion model ",
+        "abstract": "",
+        "img": "",
+    }
+]
